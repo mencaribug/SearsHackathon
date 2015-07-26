3f55bc0b609511dd3826ab7715757890aec12ea3
Michael Booth <mbooth4@gmail.com>
2015-07-26 01:09:39 -0700

    Implements client side rendering. Fixes some little bugs

--- public/javascripts/api.js
+++ public/javascripts/api.js
@@ -4,7 +4,7 @@ function addCart() {
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "/carts", true);
         xhr.onload = function () {
-            window.location.reload();
+            clientRenderCartItems();
         };
 
         xhr.setRequestHeader("Content-Type", "application/json");
@@ -14,30 +14,40 @@ function addCart() {
 
 function addItem(cartId, url) {
     if (url.length > 20 && url.indexOf("/p-") > 0) {
+        var pUrl = urlParse(url);
+        var productId = pUrl.substr(pUrl.lastIndexOf("p-") + 2);
         document.getElementById("urlInput").disabled = true;
 
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "/carts/" + cartId + "/items", true);
-        xhr.onload = function () {
-            document.getElementById("urlInput").value = "";
-            delete document.getElementById("urlInput").disabled;
-            window.location.reload();
-        };
-
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify({ url: url }));
+        addItemInternal(productId);
     }
 }
 
-function deleteItem(cartId, itemId)
-{
+function urlParse(url) {
+    if (url.indexOf("?") >= 0) {
+        return url.substring(0, url.indexOf("?"));
+    }
+
+    return url;
+}
+
+function addItemInternal(id) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/carts/" + theCartId + "/items", true);
+    xhr.onload = function () {
+        document.getElementById("urlInput").value = "";
+        document.getElementById("urlInput").disabled = false;
+        clientRenderCartItems();
+    };
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({ id: id }));
+}
+
+function deleteItem(cartId, itemId) {
     var xhr = new XMLHttpRequest();
     xhr.open("DELETE", "/carts/" + cartId + "/items/" + itemId, true);
     xhr.onload = function () {
-        setTimeout(function () {
-            window.location.reload();
-        }, 100);
-
+        clientRenderCartItems();
     };
 
     xhr.send();
@@ -49,22 +59,22 @@ function submitComment(cartId, itemId) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/carts/" + cartId + "/items/" + itemId + "/comments", true);
     xhr.onload = function () {
-        window.location.reload();
+        clientRenderCartItems();
     };
 
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({name: "anonymous", message: text}));
+    xhr.send(JSON.stringify({ name: "anonymous", message: text }));
 }
 
 function rateItem(cartId, itemId, ratingName) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/carts/" + cartId + "/items/" + itemId + "/rate", true);
     xhr.onload = function () {
-        window.location.reload(); // TODO: Reloading the page here isn't ideal
+        clientRenderCartItems();
     };
 
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({name: "anonymous", rating: ratingName}));
+    xhr.send(JSON.stringify({ name: "anonymous", rating: ratingName }));
 }
 
 function doSearch(e) {
@@ -111,10 +121,135 @@ function runSearch(text) {
             price.className = "priceDiv";
             it.appendChild(price);
 
+            var addButton = document.createElement("button");
+            addButton.innerText = "add";
+            addButton.className = "addButton";
+            addButton.onclick = function (item) {
+                addItemInternal(item.id)
+            } .bind(this, items[i]);
+            it.appendChild(addButton);
+
             container.appendChild(it);
         }
     };
 
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({query: text}));
+    xhr.send(JSON.stringify({ query: text }));
+}
+
+function getCartItems(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/cartItems/" + theCartId, true);
+    xhr.onload = function () {
+        callback(JSON.parse(this.responseText).items);
+    };
+
+    xhr.send();
+}
+
+function make(type, contents, className) {
+    var d = document.createElement(type);
+    if (contents) {
+        d.innerText = contents;
+    }
+
+    if(className) {
+        d.className = className;
+    }
+    return d;
+}
+
+function img(src) {
+    var im = document.createElement("img");
+    im.src = src;
+    return im;
+}
+
+function clientRenderCartItems() {
+    getCartItems(function (items) {
+        var container = document.getElementById("actualItemList");
+        container.textContent = "";
+        for (var i = 0; i < items.length; i++) {
+            var itemDiv = renderCartItem(items[i]);
+            container.appendChild(itemDiv);
+        }
+    });
+}
+
+function renderCartItem(item) {
+    var root = make("div", "", "item");
+    root.appendChild(img(item.img));
+    root.appendChild(make("span", item.name, "name"))
+    root.appendChild(make("span", item.price, "priceDiv"));
+    var bs = make("div", "", "bottomSection");
+    var cs = make("div", "", "commentSection");
+    for(var i = 0; i < item.comments.length; i++) {
+        cs.appendChild(make("span", item.comments[i].name + ": " + item.comments[i].message, ""));
+        cs.appendChild(make("br", "", ""));
+    }
+    var commentInput = make("input", "", "");
+    commentInput.id = "input_" + item.id;
+    commentInput.placeholder = "comment...";
+    cs.appendChild(commentInput);
+    var commentButton = make("button", "Send");
+    commentButton.onclick = function () {
+        submitComment(theCartId, item.id);
+    };
+    cs.appendChild(commentButton);
+    bs.appendChild(cs);
+
+    var rs = make("div", "", "ratingSection");
+    for(var i = 0; i < item.ratings.length; i++) {
+        var rating = item.ratings[i];
+        var rd = make("div", "", "ratingDiv");
+        var ratingButton = make("button", rating.name, "");
+        ratingButton.onclick = rateItem.bind(this, theCartId, item.id, rating.name);
+        rd.appendChild(ratingButton);
+        if  (!rating.count) {
+            rating.count = "0"
+        }
+        rd.appendChild(make("span", rating.count, "ratingCount"));
+        rs.appendChild(rd);
+    }
+    bs.appendChild(rs);
+    root.appendChild(bs);
+
+    var delButton = make("button", "Del", "delButton");
+    delButton.onclick = function () {
+        deleteItem(theCartId, item.id);
+    }
+    root.appendChild(delButton);
+    
+    return root;
+    /*
+
+             div.ratingSection
+                      for rating in item.ratings
+                        div.ratingDiv
+                          button(onclick="rateItem('" + cart.id + "', '" + item.id + "', '" + rating.name + "')")= rating.name
+                          span.ratingCount= rating.count
+                  button(onclick="deleteItem('" + cart.id + "', '" + item.id + "')") Del
+
+
+    div.item
+                  img(src=item.img)
+                  span.name= item.name
+                  span.priceDiv= item.price
+                  div
+                      a(href=item.url)= Link
+                  div.bottomSection
+                    div.commentSection
+                      for comment in item.comments
+                          span #{comment.name}: #{comment.message}
+                          br
+                      br
+                      input(id="input_" + item.id placeholder="comment...") 
+                      button(onclick="submitComment('" + cart.id + "', '" + item.id + "')") Send
+                    div.ratingSection
+                      for rating in item.ratings
+                        div.ratingDiv
+                          button(onclick="rateItem('" + cart.id + "', '" + item.id + "', '" + rating.name + "')")= rating.name
+                          span.ratingCount= rating.count
+                  button(onclick="deleteItem('" + cart.id + "', '" + item.id + "')") Del
+    */
 }
--- server.js
+++ server.js
@@ -38,22 +38,12 @@ app.get('/users', user.list);
 // Cart Management
 app.post("/carts", postCart);
 app.get("/carts/:cartId", getCart);
+app.get("/cartItems/:cartId", getCartItems);
 app.post("/carts/:cartId/items", addItemToCart);
 app.del("/carts/:cartId/items/:itemId", deleteItemFromCart);
 app.post("/carts/:cartId/items/:itemId/comments", addCommentToItem);
 app.post("/search", searchByTerm);
 app.post("/carts/:cartId/items/:itemId/rate", rateItem);
-
-// API Specification:
-// Prefix is always "/:cartId/"
-// POST /:cartId/comment/ with body
-// GET /:cartId/ returns a page (not very ajaxy)
-
-// Walkthrough
-// Create a new Cart
-// Add an item to a cart
-// View a cart
-// Vote on an item
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
@@ -64,13 +54,23 @@ function postCart(req, res) {
     res.status(201).send();
 }
 
+function getCartItems(req, res) {
+    var cartId = req.params.cartId;
+    var cart = findCartById(cartId);
+    if (cart) {
+        res.status(200).send(JSON.stringify(cart));
+    } else {
+        res.status(500).send("No cart with id " + cartId + " found.");
+    }
+}
+
 function getCart(req, res) {
     var cartId = req.params.cartId;
     var cart = findCartById(cartId);
     if (cart) {
         res.render('cart', { title: 'Shopping Buddies', cart: cart });
     } else {
-        res.send("No cart with id " + cartId + " found.");
+        res.status(500).send("No cart with id " + cartId + " found.");
     }
 }
 
@@ -82,12 +82,21 @@ function addItemToCart(req, res) {
         return;
     }
 
-    var item = makeNewItem(req.body.url);
-    cart.items.push(item);
-    scrapeProductName(item, function () {
-        saveToJson();
-        res.status(201).send();
-    });
+    if (req.body.url) {
+        var item = makeNewItem(req.body.url);
+        cart.items.push(item);
+        scrapeProductName(item, function () {
+            saveToJson();
+            res.status(201).send();
+        });
+    } else if (req.body.id) {
+        var item = makeNewItem("");
+        cart.items.push(item);
+        getProductDetails(req.body.id, item, function () {
+            saveToJson();
+            res.status(201).send();
+        });
+    }
 }
 
 function addCommentToItem(req, res) {
@@ -131,15 +140,15 @@ function rateItem(req, res) {
 
     var ratingName = req.body.rating;
     var found = false;
-    for(var i = 0; i < item.ratings.length; i++) {
+    for (var i = 0; i < item.ratings.length; i++) {
         if (item.ratings[i].name == ratingName) {
             item.ratings[i].count++;
             found = true;
         }
     }
 
-    if(!found) {
-        item.ratings.push({name: ratingName, count: 1});
+    if (!found) {
+        item.ratings.push({ name: ratingName, count: 1 });
     }
 
     res.status(200).send();
@@ -171,7 +180,17 @@ function makeNewCart(name) {
 }
 
 function makeNewItem(url) {
-    var item = { id: nextId(), ratings: [{name: "Love it", count: 0}, {name: "Hate it", count: 0}, {name: "Maybe?", count: 0}], comments: [], url: url, img: "/images/placeholder.png", name: "Uninitialized item", chatLog: [] };
+    var item = { id: nextId(),
+        ratings:
+    [{ name: "Love it", count: 0 },
+    { name: "Hate it", count: 0 },
+    { name: "Maybe?", count: 0}],
+        comments: [],
+        url: url,
+        img: "/images/placeholder.png",
+        name: "Uninitialized item",
+        chatLog: []
+    };
     return item;
 }
 
@@ -210,7 +229,14 @@ function scrapeProductName(item, callback) {
     var productId = pUrl.pathname.substr(pUrl.pathname.lastIndexOf("p-") + 2);
 
     console.log("Looking for item: '" + productId + "'");
-    GET("http://api.developer.sears.com/v2.1/products/details/sears/json/" + productId + "?apikey=pLZJ1YdF0gJQYa4w5QmqRrPb7DWWi9Rx", function (json) {
+
+    getProductDetails(productId, item, function () {
+        callback();
+    });
+}
+
+function getProductDetails(id, item, callback) {
+    GET("http://api.developer.sears.com/v2.1/products/details/sears/json/" + id + "?apikey=pLZJ1YdF0gJQYa4w5QmqRrPb7DWWi9Rx", function (json) {
         console.log("Got the response from SEARS: " + json);
         var obj = JSON.parse(json);
         try {
